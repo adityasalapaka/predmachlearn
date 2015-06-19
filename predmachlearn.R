85039c9b5255778e8eeb45ed439a0297455868cc
Aditya Salapaka <salapaka.aditya@gmail.com>
2015-06-19 16:07:00 +0530

    minor changes to script

--- predmachlearn.R
+++ predmachlearn.R
@@ -20,8 +20,6 @@ testing <- testing[-(1:7)]
 training <- training[,colSums(is.na(training)) == 0]
 testing <- testing[,colSums(is.na(testing)) == 0]
 
-#nsv <- nearZeroVar(training, saveMetrics = TRUE)
-
 dim(testing)
 dim(training)
 
@@ -31,15 +29,15 @@ inTrain <- createDataPartition(y = training$classe, p = 0.7, list = FALSE)
 subtraining <- training[inTrain,]
 subtesting <- training[-inTrain,]
 
+
 #tree model
 treeFit <- rpart(classe ~ ., method = "class", data = subtraining)
 fancyRpartPlot(treeFit, main = "Decision Tree", 
                sub = "Rpart Decision Tree To Predict Classe")
 treePredict <- predict(treeFit, subtesting, type = "class")
-confusionMatrix(treePredict, subtesting$classe)
+confusionMatrix(treePredict, subtesting$classe) #73.81
 
 #forest model
-#forestFit <- train(classe ~ ., method = "rf", data = subtraining, prox = TRUE) #why prox?
 forestFit <- randomForest(classe ~ ., data = subtraining, method = "class")
 forestPredict <- predict(forestFit, subtesting, type = "class")
-confusionMatrix(forestPredict, subtesting$classe)
+confusionMatrix(forestPredict, subtesting$classe) #99.46
